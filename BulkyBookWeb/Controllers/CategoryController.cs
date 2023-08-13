f0c4160317035909216cb1f30c96ac6d1b35d11e
Snehil Verma <snehil.verma1@inter.ikea.com>
2023-08-13 21:48:29 +0200

    Add project files.

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,50 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        //Can get Applicationdbcontext object through dependency injection because registered service in Program.cs
+        public CategoryController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categoryList = db.Categories;
+            return View(categoryList);
+        }
+
+        //Get
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Post
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            //Add custom model validation
+            //Checking if name and display order are same
+            if(category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "Name and Display order can not be same");
+            }
+
+            if(ModelState.IsValid) 
+            { 
+                db.Categories.Add(category);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ BulkyBookWeb/ReadMe.txt
@@ -0,0 +1,11 @@
+﻿This Project includes
+1. ASP.NET Core MVC (.NET 6)
+2. Entity Framework 
+3. Migrations
+4. Dependency Injection (design pattern)
+5. Model Validations
+(server side using tag helpers and ModelState)
+6. Third party Libraries (Bootstrap and Bootswatch for themes)
+
+Inspiration:
+https://www.youtube.com/watch?v=hZ1DASYd9rk&ab_channel=freeCodeCamp.org
